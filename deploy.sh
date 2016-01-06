3a255c14e72152e6313f2260876e4df753bb895c
Jo <jomogun@gmail.com>
2016-01-06 21:48:48 +0900

    rebuilding site Wed Jan  6 21:48:48 JST 2016

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,5 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:jomogoon/blog.git gh-pages
 
 echo -e "\033[0;32mDeploy change to http://jomogoon.github.io ...\033[0m"
-./hugo-build.sh
